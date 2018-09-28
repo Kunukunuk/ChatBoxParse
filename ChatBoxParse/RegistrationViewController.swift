60f32b7dc669b17a8b82b0845254f73383b182b2
Kun Huang <kkun.huang@gmail.com>
2018-09-27 20:07:36 -0400

    registration outlets and action

---
+++ ChatBoxParse/RegistrationViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RegistrationViewController.swift
+//  ChatBoxParse
+//
+//  Created by Kun Huang on 9/27/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func registerAccount(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
